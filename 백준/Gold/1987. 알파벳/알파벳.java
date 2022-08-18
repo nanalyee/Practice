6d2dcb84188837198e2c35a577a774f24d44bc8c
nanalyee <90018240+nanalyee@users.noreply.github.com>
2022-08-18 23:14:13 +0900

    [Gold IV] Title: 알파벳, Time: 1212 ms, Memory: 16648 KB -BaekjoonHub

---
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -0,0 +1,78 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	static int[] dx = {-1,1,0,0}; // 상하좌우
+	static int[] dy = {0,0,-1,1};
+	
+	static int r, c, result;
+	static char[][] map;
+	static boolean[][] visited;
+	static boolean[] alphabet;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		r = Integer.parseInt(st.nextToken());
+		c = Integer.parseInt(st.nextToken());
+		map = new char[r][c];
+		visited = new boolean[r][c];
+		alphabet = new boolean[26];
+				
+		for (int i=0; i<r; i++) {
+			String str = br.readLine();
+			for (int j=0; j<c; j++) {
+				map[i][j] = str.charAt(j);
+			}
+		}
+		
+		dfs(0,0,1);
+
+		System.out.println(result);
+	}
+
+	private static void dfs(int a, int b, int cnt) {
+		alphabet[map[a][b]-'0'-17] = true;
+		visited[a][b] = true;
+				
+		for (int i=0; i<4; i++) {
+			int x = a+dx[i];
+			int y = b+dy[i];
+			
+			if (x<0 || y<0 || x>=r || y>=c) continue; 
+			
+			int num = map[x][y] -'0' - 17;
+			if (alphabet[num] || visited[x][y]) continue;
+			dfs(x,y,cnt+1);
+		}
+		
+		result = Math.max(result, cnt);
+		//System.out.println(result);
+		//printVisited();
+
+		
+		alphabet[map[a][b]-'0'-17] = false;
+		visited[a][b] = false;
+	}
+
+	
+	// visited 확인용
+	private static void printVisited() {
+		for (int i=0; i<r; i++) {
+			for (int j=0; j<c; j++) {
+				if (visited[i][j]) System.out.print("o");
+				else  System.out.print("x");
+			}
+			System.out.println();
+		}		
+	}
+
+	
+	
+}
