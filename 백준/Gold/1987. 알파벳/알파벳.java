8a02074b13ad3e162c10791b5156cfb673725300
nanalyee <90018240+nanalyee@users.noreply.github.com>
2022-08-19 10:42:45 +0900

    [Gold IV] Title: 알파벳, Time: 956 ms, Memory: 15352 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.java
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -5,14 +5,13 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-public class Main {
+public class Main  {
 	
 	static int[] dx = {-1,1,0,0}; // 상하좌우
 	static int[] dy = {0,0,-1,1};
 	
 	static int r, c, result;
 	static char[][] map;
-	static boolean[][] visited;
 	static boolean[] alphabet;
 	
 	public static void main(String[] args) throws IOException {
@@ -22,7 +21,6 @@ public class Main {
 		r = Integer.parseInt(st.nextToken());
 		c = Integer.parseInt(st.nextToken());
 		map = new char[r][c];
-		visited = new boolean[r][c];
 		alphabet = new boolean[26];
 				
 		for (int i=0; i<r; i++) {
@@ -39,7 +37,6 @@ public class Main {
 
 	private static void dfs(int a, int b, int cnt) {
 		alphabet[map[a][b]-'0'-17] = true;
-		visited[a][b] = true;
 				
 		for (int i=0; i<4; i++) {
 			int x = a+dx[i];
@@ -48,30 +45,18 @@ public class Main {
 			if (x<0 || y<0 || x>=r || y>=c) continue; 
 			
 			int num = map[x][y] -'0' - 17;
-			if (alphabet[num] || visited[x][y]) continue;
+			if (alphabet[num]) continue;
 			dfs(x,y,cnt+1);
 		}
 		
 		result = Math.max(result, cnt);
-		//System.out.println(result);
-		//printVisited();
+
 
 		
 		alphabet[map[a][b]-'0'-17] = false;
-		visited[a][b] = false;
 	}
 
-	
-	// visited 확인용
-	private static void printVisited() {
-		for (int i=0; i<r; i++) {
-			for (int j=0; j<c; j++) {
-				if (visited[i][j]) System.out.print("o");
-				else  System.out.print("x");
-			}
-			System.out.println();
-		}		
-	}
+
 
 	
 	
