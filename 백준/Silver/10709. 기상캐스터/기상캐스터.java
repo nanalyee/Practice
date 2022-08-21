603ee11c50bcbed98d8e5283d75a1b4cae98afba
nanalyee <90018240+nanalyee@users.noreply.github.com>
2022-08-22 01:40:11 +0900

    [Silver V] Title: 기상캐스터, Time: 328 ms, Memory: 16360 KB -BaekjoonHub

---
+++ 백준/Silver/10709. 기상캐스터/기상캐스터.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main { 
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		st = new StringTokenizer(br.readLine());
+		int h = Integer.parseInt(st.nextToken());
+		int w = Integer.parseInt(st.nextToken());
+		boolean cloud = false;
+		
+		int[][] arr = new int[h][w];
+		char[][] map = new char[h][];
+		for (int i=0; i<h; i++) {
+			map[i] = br.readLine().toCharArray();
+		}
+		
+		for (int i=0; i<h; i++) {
+			cloud = false;
+			for (int j=0; j<w; j++) {
+				if (map[i][j]=='c') {
+					arr[i][j] = 0;
+					cloud = true;
+				}
+				else if (cloud) arr[i][j] = arr[i][j-1]+1;
+				else arr[i][j] = -1;
+			}
+		}
+		
+		for (int i=0; i<h; i++) {
+			for (int j=0; j<w; j++) {
+				System.out.print(arr[i][j]+" ");
+			}
+			System.out.println();
+		}
+	}
+}
