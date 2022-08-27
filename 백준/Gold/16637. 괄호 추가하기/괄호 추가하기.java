a9f652b89f5de773c90f8dbfde3b612d6bfbe50d
nanalyee <90018240+nanalyee@users.noreply.github.com>
2022-08-28 03:23:48 +0900

    [Gold IV] Title: 괄호 추가하기, Time: 128 ms, Memory: 14272 KB -BaekjoonHub

--- 백준/Gold/16637. 괄호 추가하기/괄호 추가하기.java
+++ 백준/Gold/16637. 괄호 추가하기/괄호 추가하기.java
@@ -6,27 +6,28 @@ import java.util.Arrays;
 
 public class Main {
 	
-	static ArrayList<Integer> numbers;
-	static ArrayList<Character> command;
-	static boolean[] select;
-	static int n, result;
-	static ArrayList<Integer> list;
+	static int N, result;
+	static ArrayList<Integer> numbers, A;
+	static ArrayList<Character> command, B;
+	static boolean[] selected;
+	static int[] arr;
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		n = Integer.parseInt(br.readLine());
+		N = Integer.parseInt(br.readLine());
 		String str = br.readLine();
 		
 		numbers = new ArrayList<>();
 		command = new ArrayList<>();
-		select = new boolean[n/2];
 		result = Integer.MIN_VALUE;
+
+		selected = new boolean[N/2];
+		arr = new int[N/2];
 		
-		
-		if (n==1) result = Integer.parseInt(str);
+		if (N==1) result = Integer.parseInt(str); 
 		else {
-			for (int i=0; i<n; i++) {
+			for (int i=0; i<N; i++) {
 				if (i%2==0) numbers.add(str.charAt(i)-'0');
 				else command.add(str.charAt(i));
 			}
@@ -37,79 +38,51 @@ public class Main {
 		
 		System.out.println(result);
 	}
-	
-	
-	// 괄호 부분집합
+
 	private static void subset(int idx) {
-		if (idx==n/2) {
-			//System.out.println(Arrays.toString(select));
-			list = new ArrayList<>();
-			
-			first(); // true인것부터 계산 (괄호 넣은거부터)
-			second(); // 나머지 계산
+		if (idx == N/2) {
+			//System.out.println(Arrays.toString(selected));
+			A = new ArrayList<>();
+			B = new ArrayList<>();
+			calculate();
 			return;
 		}
 		
-		if (idx==0) { // 첫 연산자는 괄호 추가 가능
-			select[idx] = true;
-			subset(idx+1);
-		}
-		if (idx!=0 && !select[idx-1]) { // 이전 연산자에 괄호가 없으면 추가 가능
-			select[idx] = true;
-			subset(idx+1);
-		}
-		select[idx] = false;
+		selected[idx] = false;
 		subset(idx+1);
+		if (idx==0 || (idx!=0 && !selected[idx-1])) {
+			selected[idx] = true;
+			subset(idx+1);
+		}
 	}
-	
-	
 
-
-
-	// 괄호 순서대로 연산하기
-	private static void first() {
-		for (int i=0; i<n/2; i++)  {
-			int num = 0;
-			if (select[i]) {
-				char com = command.get(i); // 연산자 불러오기
-				if (com=='+') {
-					num = numbers.get(i) + numbers.get(i+1);
-				}
-				else if (com=='-') {
-					num = numbers.get(i) - numbers.get(i+1);
-				}
-				else if (com=='*') {
-					num = numbers.get(i) * numbers.get(i+1);
-				}
-				list.add(num);
+	private static void calculate() {
+		
+		for (int i=0; i<N/2; i++) {
+			char cmd = command.get(i);
+			if (selected[i]) {
+				int num = 0;
+				if (cmd=='+') num = numbers.get(i) + numbers.get(i+1);
+				else if (cmd=='-') num = numbers.get(i) - numbers.get(i+1);
+				else if (cmd=='*') num = numbers.get(i) * numbers.get(i+1);
+				A.add(num);
 			}
 			else {
-				if (i==0 && !select[i]) list.add(numbers.get(i));
-				if (i!=n/2-1 && !select[i+1]) list.add(numbers.get(i+1)); 
-				if (i==n/2-1) list.add(numbers.get(i+1));
+				if (i==0) A.add(numbers.get(0));
+				if (i==N/2-1 || !selected[i+1]) A.add(numbers.get(i+1));
+				B.add(cmd);
 			}
 		}
-		//System.out.println(list);
-	}
-	
-	// 괄호를 계산한 후의 나머지 연산
-	private static void second() {
-		int num = list.get(0);
-		int idx=0;
-		for (int i=0; i<n/2; i++)  {
-			if (!select[i]) {
-				char com = command.get(i); // 연산자 불러오기
-				if (com=='+') {
-					num += list.get(idx+1);
-				}
-				else if (com=='-') {
-					num -= list.get(idx+1);
-				}
-				else if (com=='*') {
-					num *= list.get(idx+1);
-				}
-				idx++;
-			}
+		//System.out.println(A);
+		//System.out.println(B);
+		
+		int size = B.size();
+		int num = A.get(0);
+		for (int i=0; i<size; i++) {
+			char cmd = B.get(i);
+			if (cmd=='+') num += A.get(i+1);
+			else if (cmd=='-') num -= A.get(i+1);
+			else if (cmd=='*') num *= A.get(i+1);
 		}
 		//System.out.println(num);
 		result = Math.max(result, num);
